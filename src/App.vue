ea140d9529d80750733239325549c3493ba76c6f
= <=>
2018-08-03 11:06:47 +0530

    added vue-headroom.js and semantic ui for navbar and layout respectively

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    
+    <Navbar></Navbar>
     <Header title="This is the Header"></Header>
     <Banner title="This is banner"> </Banner>
     <About title="This is About Section"> </About>
@@ -14,10 +14,12 @@ import Header from './components/Header/Header.vue'
 import Banner from './components/Banner/Banner.vue'
 import About from './components/About/About.vue'
 import Projects from './components/Projects/Projects.vue'
+import Navbar  from './components/Navbar/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
+    Navbar,
     Header,
     Banner,
     About,
---
+++ src/components/Navbar/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+    <headroom>
+        <header>
+            HAHAHHAHA
+        </header>
+  </headroom>
+</template>
+
+<script>
+import { headroom } from "vue-headroom";
+export default {
+    components: {
+        headroom
+    },
+    name: 'Navbar',
+    props: {
+                // when element is initialised
+        initial : "headroom",
+        // when scrolling up
+        pinned : "headroom--pinned",
+        // when scrolling down
+        unpinned : "headroom--unpinned",
+        // when above offset
+        top : "headroom--top",
+        // when below offset
+        notTop : "headroom--not-top",
+        // when at bottom of scoll area
+        bottom : "headroom--bottom",
+        // when not at bottom of scroll area
+        notBottom : "headroom--not-bottom"
+    }
+}
+</script>
+
+<style scoped>
+.headroom {
+    padding: 50px;
+}
+.headroom--not-top {
+    background: #00a699;
+}
+</style>
+
+
